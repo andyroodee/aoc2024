dbdcbed5d0a766eb3c8c91d128fa289185a1bcde
andyroodee <andrew.doane@gmail.com>
2024-12-06 12:39:34 -0400

    Making it a tiny bit faster

--- day6/b.py
+++ day6/b.py
@@ -24,7 +24,29 @@ def change_dir(current_dir):
     if current_dir == down: return left
     if current_dir == left: return up
 
-def detect_cycle(grid, start_pos, start_dir):   
+def get_path(grid, start_pos, start_dir):
+    pos = start_pos
+    dir = start_dir 
+    visited = set()
+    visited.add(pos)
+
+    while True:
+        # try to move in the given dir    
+        next = (pos[0] + dir[0], pos[1] + dir[1])
+        # check for out of bounds, which means we're done
+        if next[0] >= bounds[0] or next[0] < 0 or next[1] >= bounds[1] or next[1] < 0:
+            break
+
+        # adjust dir if this is an obstacle
+        if grid[next[0]][next[1]] == '#':
+            dir = change_dir(dir)
+        else:   
+            pos = next
+            visited.add(pos)
+    
+    return visited
+
+def detect_cycle(grid, start_pos, start_dir):
     pos = start_pos
     dir = start_dir 
     visited = set()
@@ -47,15 +69,16 @@ def detect_cycle(grid, start_pos, start_dir):
                 return True
             visited.add((pos, dir))
 
+original_path = get_path(grid, pos, dir)
 count = 0
-for i in range(len(grid)):
-    for j in range(len(grid[i])):
-        if (i, j) == pos:
-            continue
-        g = grid[:]
-        if grid[i][j] != '#':
-            g[i] = g[i][:j] + '#' + g[i][j+1:]
-        if detect_cycle(g, pos, dir):
-            count += 1
+for place in original_path:
+    (i, j) = place
+    if (i, j) == pos:
+        continue
+    g = grid[:]
+    if grid[i][j] != '#':
+        g[i] = g[i][:j] + '#' + g[i][j+1:]
+    if detect_cycle(g, pos, dir):
+        count += 1
 
 print(count)
